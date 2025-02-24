9a384649b90409064f85e68719944d8233018d40
shrey <shreya272002gupta@gmail.com>
2025-02-24 10:19:48 +0530

    [Shreya] Add. Setup for Spring messaging app

---
+++ src/test/java/com/example/springMessagingApp/SpringMessagingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springMessagingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMessagingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
